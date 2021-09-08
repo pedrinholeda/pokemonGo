eb0049617f5c9cc16207e92d4255b20c9b5bdb63
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-09-08 16:27:30 -0300

    created map

---
+++ Pokemon Go/Pokemon Go/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Pokemon Go
+//
+//  Created by Pedro Léda on 08/09/21.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    
+    // MARK: Outlets
+    @IBOutlet weak var map: MKMapView!
+    
+    // MARK: Initialization
+    
+    // MARK: Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Actions
+    
+    // MARK: Methods
+
+
+}
+
