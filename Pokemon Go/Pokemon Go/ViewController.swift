71ed892b74223525779b328151fbb89f850ba073
Pedro Leda <pedro.henrique.leda@gmail.com>
2021-09-09 16:04:53 -0300

    add random notations in user map

--- Pokemon Go/Pokemon Go/ViewController.swift
+++ Pokemon Go/Pokemon Go/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 
     // MARK: Properties
     var locationManeger = CLLocationManager()
+    var count = 0
     
     // MARK: Outlets
     @IBOutlet weak var map: MKMapView!
@@ -26,11 +27,52 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManeger.delegate = self
         locationManeger.requestWhenInUseAuthorization()
         locationManeger.startUpdatingLocation()
+        
+        //pokemons
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { (timer) in
+            if let cordenadas = self.locationManeger.location?.coordinate {
+                let notation = MKPointAnnotation()
+                notation.coordinate = cordenadas
+                
+                let latAleatoria = (Double(arc4random_uniform(500)) - 250) / 100000.0
+                let longAleatoria = (Double(arc4random_uniform(500)) - 250) / 100000.0
+                
+                notation.coordinate.latitude += latAleatoria
+                notation.coordinate.longitude += longAleatoria
+                
+                self.map.addAnnotation(notation)
+            }
+        }
     }
     
     // MARK: Actions
+    @IBAction func centerPlayer(_ sender: Any) {
+        self.center()
+    }
+    
+    @IBAction func openPokedex(_ sender: Any) {
+    }
+    
     
     // MARK: Methods
+    func center() {
+        if let coordinate = locationManeger.location?.coordinate {
+            let region = MKCoordinateRegion(center: coordinate,
+                                            latitudinalMeters: 200,
+                                            longitudinalMeters: 200)
+            map.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.center()
+        if count < 3 {
+            count += 1
+        }else{
+            locationManeger.stopUpdatingLocation()
+        }
+    }
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status != .authorizedWhenInUse && status != .notDetermined {
             let alertController = UIAlertController(title: "Permissão de Autorização",
